4a94cae2d59da237b62cf2a7bbe4e41aef577c95
Li-Peiyan <87517767+Li-Peiyan@users.noreply.github.com>
2021-11-05 13:50:17 +0800

    Create OptMatrix.c

---
+++ week6/ch03/OptMatrix.c
@@ -0,0 +1,18 @@
+void OptMatrix( const long c[], int N, TwoDimArray M, TwoDimArray LastChange )
+{ int i, k, Left, Right; 
+}
+long ThisM;
+  for( Left= l; Left <= N; Left++ 
+      
+M[ Left ][ Left ] =0;
+   for( k = i; k< N; k++) 
+    for( Left = 1; Left<= N - k; Leftr++)
+     Right = Left+ k ;
+  M[ Left ][ Right ] = Infinity;
+  
+for( i =Left; i < Right; i++ )
+ThisM = M[ _Left ][ i]+M[i +1 ][ Right ]
++cl Left - 1]* CL i ]*C[ Right ];if( ThisM 之 M[ Left ][ Right ] )
+{
+M[Left ][ Right ] = ThisM;
+LastChange[ Left j[ Right ] = i;
