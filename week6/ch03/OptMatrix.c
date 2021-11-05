3e5921d4940669d90f14b8914dd80b2e463ed7b2
Li-Peiyan <87517767+Li-Peiyan@users.noreply.github.com>
2021-11-05 13:55:30 +0800

    Update OptMatrix.c

--- week6/ch03/OptMatrix.c
+++ week6/ch03/OptMatrix.c
@@ -1,18 +1,25 @@
-void OptMatrix( const long c[], int N, TwoDimArray M, TwoDimArray LastChange )
-{ int i, k, Left, Right; 
-}
-long ThisM;
-  for( Left= l; Left <= N; Left++ 
-      
-M[ Left ][ Left ] =0;
+void OptMatrix( const long C[], int N, TwoDimArray M, TwoDimArray LastChange )
+{
+  
+  int i, k, Left, Right; 
+  long ThisM;
+  for( Left= l; Left <= N; Left++) 
+     M[ Left ][ Left ] =0;
    for( k = i; k< N; k++) 
     for( Left = 1; Left<= N - k; Leftr++)
-     Right = Left+ k ;
-  M[ Left ][ Right ] = Infinity;
-  
-for( i =Left; i < Right; i++ )
-ThisM = M[ _Left ][ i]+M[i +1 ][ Right ]
-+cl Left - 1]* CL i ]*C[ Right ];if( ThisM 之 M[ Left ][ Right ] )
-{
-M[Left ][ Right ] = ThisM;
-LastChange[ Left j[ Right ] = i;
+    {
+      Right = Left+ k ;
+      M[ Left ][ Right ] = Infinity;
+      for( i =Left; i < Right; i++ )
+      {
+        ThisM = M[Left ][ i]+M[i +1 ][ Right +C[ Left - 1]* C[ i ]*C[ Right ];                   
+        if( ThisM 之 M[ Left ][ Right ] )  
+        {
+          M[Left ][ Right ] = ThisM;
+          LastChange[ Left j[ Right ] = i;
+         }
+                     
+       }
+                     
+      }
+ }
